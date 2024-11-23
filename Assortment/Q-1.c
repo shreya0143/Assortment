4aa3041b52d16d5eb3f7cb99d197330911c73360
shreya0143 <parmarshreya192@gmail.com>
2024-11-23 13:14:15 +0530

    Add files via upload

---
+++ Assortment/Q-1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n,i;
+
+    
+    printf("Enter the array's size: ");
+    scanf("%d", &n);
+
+    int arr[n];  
+
+    
+    printf("Enter array's elements:\n");
+    for ( i = 0; i < n; i++) {
+        printf("a[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printf("Negative elements from the Array: ");
+    int found = 0;  
+    for ( i = 0; i < n; i++) {
+        if (arr[i] < 0) {
+            if (found) {
+                printf(", ");  
+            }
+            printf("%d", arr[i]);
+            found = 1;
+        }
+    }
+
+    
+    if (!found) {
+        printf("None");
+    }
+
+    return 0;
+}
+
